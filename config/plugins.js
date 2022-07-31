c80e11898aebbc2ebef5d7bbf34b7047015ab26b
bogdandraghia <bgd.draghia@gmail.com>
2022-07-31 17:20:48 +0000

    add aws-s3 provider plugin

---
+++ config/plugins.js
@@ -0,0 +1,26 @@
+module.exports = ({ env }) => ({
+    slugify: {
+      enabled: true,
+      config: {
+        contentTypes: {
+          article: {
+            field: 'slug',
+            references: 'title',
+          },
+        },
+      },
+    },
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          accessKeyId: env('AWS_ACCESS_KEY_ID'),
+          secretAccessKey: env('AWS_ACCESS_SECRET'),
+          region: env('AWS_REGION'),
+          params: {
+            Bucket: env('AWS_BUCKET'),
+          },
+        },
+      },
+    },
+  });
